3f032ecf4f3ad4c2aa4d3a536a3b2345e9a78781
Anirudh Meshram <meshramanirudh.work@gmail.com>
2026-02-04 11:50:53 +0000

    Initial commit

---
+++ src/deloitte_job_scraper/job_links.py
@@ -0,0 +1,39 @@
+# Module responsible for generating an exhaustive list of all the jobs listed on usijobs.deloitte.com
+
+import requests
+from bs4 import BeautifulSoup
+import math
+import sys
+
+
+def getJobLinks() -> list[str]:
+    links = []
+    link = "https://usijobs.deloitte.com/en_US/careersUSI/SearchJobs/?jobRecordsPerPage=10&jobOffset="
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, "lxml")
+    noOfJobs = int(soup.find("span", class_="jobListTotalRecords").text)
+
+    print(f"Found {noOfJobs} jobs.")
+
+    # Find all job links
+
+    for i in range(0, (math.floor(noOfJobs / 10) * 10) + 1, 10):
+        response = requests.get(link + str(i))
+        soup = BeautifulSoup(response.text, "lxml")
+        jobsOnPage = soup.find_all("a", class_="link")
+        for jobLink in jobsOnPage:
+            if str(jobLink.get("href")).startswith(
+                "https://usijobs.deloitte.com/en_US/careersUSI/JobDetail/"
+            ):
+                links.append(str(jobLink.get("href")))
+
+        # Progress
+        sys.stdout.write(f"\r Gathering Links : {len(links)}/{noOfJobs}")
+        sys.stdout.flush()
+
+    print()
+    return links
+
+
+if __name__ == "__main__":
+    getJobLinks()
---
+++ src/deloitte_job_scraper/main.py
@@ -0,0 +1,43 @@
+from job_links import getJobLinks
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import sys
+import os
+
+links = getJobLinks()
+jobs = dict()
+
+for link in links:
+    page = requests.get(link)
+    soup = BeautifulSoup(page.text, "lxml")
+
+    jobDescriptionIdentifier = "container--boxed"
+    jobDescription = soup.find("div", class_="container--boxed").text
+
+    jobTitleIdentifier = "article__header__text__title--4"
+    jobTitle = soup.find("h2", class_=jobTitleIdentifier).text
+
+    # Job Requisition Code
+    jobCode = str(link[link.rfind("/") + 1 :])
+
+    jobs[jobCode] = {
+        "jobTitle": jobTitle.strip(),
+        "jobDescription": jobDescription.strip(),
+        "jobApplyLink": f"https://usijobs.deloitte.com/en_US/careersUSI/Login?jobId={jobCode}",
+    }
+
+    # Progress
+    sys.stdout.write(
+        f"\r Details for Job ID {jobCode} added : {len(jobs.keys())}/{len(links)}"
+    )
+    sys.stdout.flush()
+
+print()
+
+filename = "deloitte_jobs"
+pd.DataFrame(jobs).T.reset_index().rename(columns={"index": "jobId"}).to_csv(
+    f"{filename}.csv"
+)
+
+print(f"Saved {filename}.csv in {os.path.abspath('.')}")
